bf81dbeb7acdb37015e09b5105e94feb33e62443
Mohamed <Mohamed_Mohamed-Ali@hotmail.com>
2020-03-14 16:38:04 +0100

    elimia riproduzione

--- spootify/src/main/java/it/spootify/spootify/web/rest/resources/RiproduzioneResource.java
+++ spootify/src/main/java/it/spootify/spootify/web/rest/resources/RiproduzioneResource.java
@@ -2,7 +2,10 @@ package it.spootify.spootify.web.rest.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,8 @@ import it.spootify.spootify.model.Brano;
 import it.spootify.spootify.model.Riproduzione;
 import it.spootify.spootify.service.RiproduzioneService;
 import it.spootify.spootify.web.dto.BranoDTO;
+import it.spootify.spootify.web.dto.ConfermaDTO;
+import it.spootify.spootify.web.dto.DTO;
 import it.spootify.spootify.web.dto.RiproduzioneDTO;
 import it.spootify.spootify.web.dto.RiproduzioneProvaDTO;
 
@@ -33,7 +38,7 @@ public class RiproduzioneResource {
 		return ResponseEntity.ok(riproduzioneDTO);
 	}
 	
-	@PostMapping("/cambiaBrano")
+	@PutMapping("/cambiaBrano")
 	public ResponseEntity<BranoDTO> cambiaBrano(@RequestBody RiproduzioneProvaDTO input){
 		
 		BranoDTO branoDTO = riproduzioneService.cambiaBrano(input).buildDTO(false, false, false);
@@ -41,4 +46,9 @@ public class RiproduzioneResource {
 		return ResponseEntity.ok(branoDTO);
 	}
 	
+	@DeleteMapping("/elimina/{id}")
+	public ResponseEntity<DTO> elimina(@PathVariable Long id){
+		riproduzioneService.elimina(id);
+		return ResponseEntity.ok(new ConfermaDTO("Riproduzione ["+id+"] eliminata con successo"));
+	}
 }
